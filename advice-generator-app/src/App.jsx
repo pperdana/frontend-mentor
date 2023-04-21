c9e9d12828846be453dc1f2218df88ff222fc6ed
Panji <panji9502@gmail.com>
2023-04-21 20:08:47 +0700

    init files | advice generator

---
+++ advice-generator-app/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <main>
+      <section className="advice-container">
+        <h1 className="advice-title">advice #117</h1>
+        <p className="advice-text">
+          &quot;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+          quo necessitatibus nihil id&quot;
+        </p>
+        <div className="divider">-----------</div>
+        <div className="advice-btn">
+          <button className="btn">TESTT</button>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
