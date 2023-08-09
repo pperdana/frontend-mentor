8f8cfb4f7889d15423c95d2b099fbf4993d70840
Panji <panji9502@gmail.com>
2023-08-09 11:08:30 +0700

    add group and post property in data.js

--- notifications-page/src/Person.jsx
+++ notifications-page/src/Person.jsx
@@ -1,12 +1,13 @@
-const Person = ({ name, status, id, posted, src, alt }) => {
+const Person = ({ post, group, name, status, id, sent, src, alt }) => {
   return (
     <div className="card">
       <img className="image" src={src} alt={alt} />
       <div className="info">
         <div className="desc">
-          <h2 className="name">{name}</h2> <p className="stats">{status}</p>
+          <h2 className="name">{name}</h2> <p className="stats">{status}</p>{" "}
+          <strong>{post || group}</strong>
         </div>
-        <div className="time">{posted}</div>
+        <div className="time">{sent}</div>
       </div>
     </div>
   );
