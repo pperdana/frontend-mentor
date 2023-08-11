f11a8d5909582192710a436b82edae4064e9d72f
Panji <panji9502@gmail.com>
2023-08-11 10:41:51 +0700

    add active state

--- notifications-page/src/Person.jsx
+++ notifications-page/src/Person.jsx
@@ -20,16 +20,8 @@ const Person = ({
         <div className="info">
           <div className="desc">
             <h2 className="name">{name}</h2> <p className="stats">{status}</p>{" "}
-            {post && (
-              <strong className="post" style={{ color: "hsl(219, 12%, 42%)" }}>
-                {post}
-              </strong>
-            )}
-            {group && (
-              <strong className="group" style={{ color: "hsl(219, 85%, 26%)" }}>
-                {group}
-              </strong>
-            )}
+            {post && <strong className="post">{post}</strong>}
+            {group && <strong className="group">{group}</strong>}
             {read && <span className={read ? "dot display-dot" : "dot"} />}
           </div>
           <div className="time">{sent}</div>
