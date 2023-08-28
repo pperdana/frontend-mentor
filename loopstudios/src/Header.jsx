be977365fc5ca77408829be017a8dcd237cad992
Panji <panji9502@gmail.com>
2023-08-28 15:23:07 +0700

    add header components

---
+++ loopstudios/src/Header.jsx
@@ -0,0 +1,58 @@
+const Header = () => {
+  return (
+    <header>
+      <a href="#">
+        <img
+          className="logo"
+          alt="Loopstuios logo"
+          src="/src/assets/images/logo.svg"
+        />
+      </a>
+      <nav className="main-nav">
+        <ul className="main-nav-list">
+          <li>
+            <a className="main-nav-link" href="#about">
+              about
+            </a>
+          </li>
+          <li>
+            <a className="main-nav-link" href="#careers">
+              careers
+            </a>
+          </li>
+          <li>
+            <a className="main-nav-link" href="#events">
+              events
+            </a>
+          </li>
+          <li>
+            <a className="main-nav-link" href="#products">
+              products
+            </a>
+          </li>
+          <li>
+            <a className="main-nav-link" href="#support">
+              support
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {/* <button className="btn-mobile-nav">
+     <ion-icon
+       className="icon-mobile-nav md hydrated"
+       name="menu-outline"
+       role="img"
+       aria-label="menu outline"
+     ></ion-icon>
+     <ion-icon
+       className="icon-mobile-nav md hydrated"
+       name="close-outline"
+       role="img"
+       aria-label="close outline"
+     ></ion-icon>
+    </button> */}
+    </header>
+  );
+};
+
+export default Header;
