7c96106adc0dd184c61965f6527c760cb8c0c8ca
Panji <panji9502@gmail.com>
2023-10-17 06:34:42 +0700

    style hero section

--- huddle-landing-page/src/Hero.jsx
+++ huddle-landing-page/src/Hero.jsx
@@ -1,8 +1,9 @@
 import mockups from "./images/illustration-mockups.svg";
+
 const Hero = () => {
   return (
-    <section className="section-hero">
-      <div className="hero">
+    <section className="section-hero mt-[1.2rem]">
+      <div className="hero my-0 mx-auto py-0 px-[7.3rem] flex gap-[2rem] max-w-[120rem]	">
         <div className="hero-img-box">
           <img
             src={mockups}
@@ -10,16 +11,18 @@ const Hero = () => {
             className="mockups"
           />
         </div>
-        <div className="hero-text-box">
-          <h1 className="hero__heading">
+        <div className="hero-text-box w-[30rem]">
+          <h1 className="hero__heading text-[2.35rem] mt-[3rem]">
             Build The Community Your Fans Will Love
           </h1>
-          <p className="hero__content">
+          <p className="hero__content mt-[1rem] mb-[2rem]">
             Huddle re-imagines the way we build communities. You have a voice,
             but so does your audience. Create connections with your users as you
             engage in genuine discussion.
           </p>
-          <a className="btn">Register</a>
+          <a className="btn inline-flex	text-base bg-white py-[0.85rem] px-[3.5rem] rounded-[100px] shadow-[0_3px_10px_0px_rgba(0,0,0,0.5)] transition-all duration-300	focus:text-white focus:cursor-pointer hover:text-white hover:cursor-pointer">
+            Register
+          </a>
         </div>
       </div>
     </section>
