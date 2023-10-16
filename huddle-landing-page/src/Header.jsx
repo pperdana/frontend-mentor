f7636def3b018247b5ce823e26f27a587a9325be
Panji <panji9502@gmail.com>
2023-10-16 22:01:40 +0700

    style logo

--- huddle-landing-page/src/Header.jsx
+++ huddle-landing-page/src/Header.jsx
@@ -2,7 +2,7 @@ import logo from "./images/logo.svg";
 const Header = () => {
   return (
     <header className="header h-[7.6rem] px-[7rem] flex align-items-center;">
-      <img src={logo} alt="Huddle Logo" className="logo" />
+      <img src={logo} alt="Huddle Logo" className="logo w-[11.5rem]" />
     </header>
   );
 };
