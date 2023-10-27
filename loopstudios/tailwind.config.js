e9036add5b516fe7f613e09f8dd36142b10188e1
Panji <panji9502@gmail.com>
2023-10-27 15:47:59 +0700

    add gallery background

--- loopstudios/tailwind.config.js
+++ loopstudios/tailwind.config.js
@@ -5,6 +5,14 @@ export default {
     extend: {
       backgroundImage: {
         "hero-pattern": "url('/src/assets/images/desktop/image-hero.jpg')",
+        earth: "url('assets/images/desktop/image-deep-earth.jpg')",
+        arcade: "url('assets/images/desktop/image-night-arcade.jpg')",
+        soccer: "url('assets/images/desktop/image-soccer-team.jpg')",
+        grid: "url('assets/images/desktop/image-grid.jpg')",
+        above: "url('assets/images/desktop/image-from-above.jpg')",
+        borealis: "url('assets/images/desktop/image-pocket-borealis.jpg')",
+        curiosity: "url('assets/images/desktop/image-curiosity.jpg')",
+        fisheye: "url('assets/images/desktop/image-fisheye.jpg')",
       },
     },
   },
