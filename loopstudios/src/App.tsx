cc1f24de87293b3ebb090801317b85957b7f3c0f
Panji <panji9502@gmail.com>
2023-10-27 15:46:26 +0700

    update style

--- loopstudios/src/App.tsx
+++ loopstudios/src/App.tsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <>
       {/* <Hero /> */}
-      <Info />
-      {/* <Feature /> */}
+      {/* <Info /> */}
+      <Feature />
       {/* <Footer /> */}
     </>
   );
--- loopstudios/src/Feature.tsx
+++ loopstudios/src/Feature.tsx
@@ -1,54 +1,62 @@
+import deepEarth from "./assets/images/desktop/image-deep-earth.jpg";
+
 const Feature = () => {
   return (
-    <div className="page">
-      <div className="wrapper">
+    <div className="page flex justify-center">
+      <div className="wrapper max-w-[calc((1vh+1vw)*52.2)] w-full py-0 px-[30px]">
         <div className="portfolio">
-          <header className="portfolio__header">
-            <h3 className="portfolio__heading">Our creations</h3>
-            <button className="portfolio__button">See all</button>
+          <header className="portfolio__header flex justify-between	items-center	mb-[calc((1vh+1vw)*4)]">
+            <h3 className="portfolio__heading text-[calc((1vh+1vw)*2.13)] leading-[1.1] font-['Josefin_Sans'] font-normal	uppercase">
+              Our creations
+            </h3>
+            <button className="portfolio__button bg-[hsl(0,0%,100%)] text-[hsl(0,0%,0%)] border-[1px] border-solid	border-[hsl(0,0%,0%)] uppercase py-[10px] px-[30px] font-[calc((1vh+1vw)*0.67)] leading-[1] font-['Alata'] font-normal tracking-[2px] duration-[400ms]">
+              See all
+            </button>
           </header>
-          <div className="gallery">
-            <div className="card _earth">
+          <div className="gallery flex flex-wrap	justify-between	gap-[calc((1vh+1vw)*1.2)] mb-[calc((1vh+1vw)*8)]">
+            <div className="bg_gallery bg-earth">
               <div className="card__content">
                 <h3 className="card__name">Deep earth</h3>
               </div>
             </div>
-            <div className="card _arcade">
+            <div className="bg_gallery bg-arcade">
               <div className="card__content">
                 <h3 className="card__name">Night arcade</h3>
               </div>
             </div>
-            <div className="card _soccer">
+            <div className="bg_gallery bg-soccer">
               <div className="card__content">
                 <h3 className="card__name">Soccer team VR</h3>
               </div>
             </div>
-            <div className="card _grid">
+            <div className="bg_gallery bg-grid">
               <div className="card__content">
                 <h3 className="card__name">The grid</h3>
               </div>
             </div>
-            <div className="card _vr">
+            <div className="bg_gallery bg-above">
               <div className="card__content">
                 <h3 className="card__name">From up above VR</h3>
               </div>
             </div>
-            <div className="card _borealis">
+            <div className="bg_gallery bg-borealis">
               <div className="card__content">
                 <h3 className="card__name">Pocket borealis</h3>
               </div>
             </div>
-            <div className="card _curiosity">
+            <div className="bg_gallery bg-curiosity">
               <div className="card__content">
                 <h3 className="card__name">The curiosity</h3>
               </div>
             </div>
-            <div className="card _fisheye">
+            <div className="bg_gallery bg-fisheye">
               <div className="card__content">
                 <h3 className="card__name">Make it fisheye</h3>
               </div>
             </div>
-            <button className="portfolio__button _mobile">See all</button>
+            <button className="portfolio__button _mobile hidden ">
+              See all
+            </button>
           </div>
         </div>
       </div>
