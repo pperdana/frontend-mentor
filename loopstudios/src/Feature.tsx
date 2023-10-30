6bceb0d81f99357954a9052bcea1aaf4e3c482c0
Panji <panji9502@gmail.com>
2023-10-30 14:00:35 +0700

    update feature font

--- loopstudios/src/Feature.tsx
+++ loopstudios/src/Feature.tsx
@@ -6,10 +6,10 @@ const Feature = () => {
       <div className="wrapper max-w-[calc((1vh+1vw)*52.2)] w-full py-0 px-[30px]">
         <div className="portfolio">
           <header className="portfolio__header flex justify-between	items-center	mb-[calc((1vh+1vw)*4)]">
-            <h3 className="portfolio__heading text-[calc((1vh+1vw)*2.13)] leading-[1.1] font-['Josefin_Sans'] font-normal	uppercase">
+            <h3 className="portfolio__heading text-[calc((1vh+1vw)*2.13)] leading-[1.1] font-josefin font-[300]	uppercase">
               Our creations
             </h3>
-            <button className="portfolio__button bg-[hsl(0,0%,100%)] text-[hsl(0,0%,0%)] border-[1px] border-solid	border-[hsl(0,0%,0%)] uppercase py-[10px] px-[30px] font-[calc((1vh+1vw)*0.67)] leading-[1] font-['Alata'] font-normal tracking-[2px] duration-[400ms]">
+            <button className="portfolio__button bg-[hsl(0,0%,100%)] text-[hsl(0,0%,0%)] border-[1px] border-solid	border-[hsl(0,0%,0%)] uppercase py-[10px] px-[30px] font-[calc((1vh+1vw)*0.67)] leading-[1] font-alata font-normal tracking-[2px] duration-[400ms]">
               See all
             </button>
           </header>
