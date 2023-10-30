e78822dc1e4d7cea2923d4c7ce59bdeae0fe1680
Panji <panji9502@gmail.com>
2023-10-30 13:56:16 +0700

    update info font

--- loopstudios/src/Info.tsx
+++ loopstudios/src/Info.tsx
@@ -11,10 +11,10 @@ const Info = () => {
             className="introduce__image absolute w-[calc((1vh+1vw)*34)]"
           />
           <div className="introduce__text top-[calc((1vh+1vw)*9)] left-[calc((1vh+1vw)*24)] relative py-[calc((1vh+1vw)*4.27)] pr-0 pl-[calc((1vh+1vw)*4.27)] w-[48%] bg-[hsl(0,0%,100%)]">
-            <h2 className="introduce__heading text-[calc((1vh+1vw)*2.13)] leading-[1.1] font-['josefin-sans'] font-normal	uppercase">
+            <h2 className="introduce__heading text-[calc((1vh+1vw)*2.13)] leading-[1.1] font-josefin font-[300]	uppercase">
               The leader in interactive VR
             </h2>
-            <p className="introduce__paragraph font-['alata'] mt-[30px] leading-[1.5] text-[hsl(0,0%,55%)] text-[calc((1vh+1vw)*0.67)]">
+            <p className="introduce__paragraph font-alata mt-[30px] leading-[1.5] text-[hsl(0,0%,55%)] text-[calc((1vh+1vw)*0.67)]">
               Founded in 2011, Loopstudios has been producing world-class
               virtual reality projects for some of the best companies around the
               globe. Our award-winning creations have transformed businesses
