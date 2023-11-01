eade7319983f31462b09ccaca7ae63841f3fda0c
Panji <panji9502@gmail.com>
2023-11-01 19:32:47 +0700

    add hero background for mobile

--- loopstudios/tailwind.config.js
+++ loopstudios/tailwind.config.js
@@ -4,7 +4,8 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/images/desktop/image-hero.jpg')",
+        hero: "url('/src/assets/images/desktop/image-hero.jpg')",
+        "hero-mobile": "url('/src/assets/images/mobile/image-hero.jpg')",
         earth: "url('assets/images/desktop/image-deep-earth.jpg')",
         arcade: "url('assets/images/desktop/image-night-arcade.jpg')",
         soccer: "url('assets/images/desktop/image-soccer-team.jpg')",
