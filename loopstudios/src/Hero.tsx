dbb82264e3f2226eb96938e88aa05e7404c874e1
Panji <panji9502@gmail.com>
2023-11-01 19:33:31 +0700

    add md screen style

--- loopstudios/src/Hero.tsx
+++ loopstudios/src/Hero.tsx
@@ -2,9 +2,9 @@ import logo from "./assets/images/logo.svg";
 
 const Hero = () => {
   return (
-    <div className="page bg-hero-pattern bg-no-repeat	bg-cover bg-center">
+    <div className="page bg-hero bg-no-repeat	bg-cover bg-center md:bg-hero-mobile">
       <div className="wrapper">
-        <div className="hero flex flex-col	justify-between	items-start">
+        <div className="hero flex flex-col	justify-between	items-start md:h-screen	md:block">
           <header className="hero__header mt-[calc((1vh+1vw)*2.76)] flex justify-between  items-center w-full">
             <img
               src={logo}
@@ -29,7 +29,7 @@ const Hero = () => {
               </a>
             </nav>
             <button
-              className="hamburger hamburger--emphatic overflow-hidden	py-[15px] px-[15px] hidden cursor-pointer	z-[3] transition-opacity	transition-[filter] duration-[150ms] ease-linear	text-inherit	normal-case	bg-transparent	border-0 m-0 overflow-visible"
+              className="hamburger hamburger--emphatic overflow-hidden	py-[15px] px-[15px] hidden cursor-pointer	z-[3] transition-opacity	transition-[filter] duration-[150ms] ease-linear	text-inherit	normal-case	bg-transparent	border-0 m-0 overflow-visible md:inline-block"
               type="button"
             >
               <span className="hamburger-box">
@@ -37,11 +37,11 @@ const Hero = () => {
               </span>
             </button>
           </header>
-          <div className="hero__heading text-[hsl(0,0%,100%)] font-josefin font-light	uppercase border-[2px]	border-solid	border-[hsl(0,0%,100%)] text-[calc((1vh+1vw)*3.2)] w-[calc((1vh+1vw)*31)] p-[30px] leading-[1.1] mt-[calc((1vh+1vw)*5.4)] mb-[calc((1vh+1vw)*6.5)] mx-0">
+          <div className="hero__heading text-[hsl(0,0%,100%)] font-josefin font-light	uppercase border-[2px]	border-solid	border-[hsl(0,0%,100%)] text-[calc((1vh+1vw)*3.2)] w-[calc((1vh+1vw)*31)] p-[30px] leading-[1.1] mt-[calc((1vh+1vw)*5.4)] mb-[calc((1vh+1vw)*6.5)] mx-0 md:mt-[calc((1vh+1vw)*22)] md:mb-0 md:mx-auto md:w-[calc((1vh+1vw)*26)] md:text-[calc((1vh+1vw)*3)]">
             Immersive experiences that deliver
           </div>
         </div>
-        <div className="menu closed left-[-1000px] duration-[400ms] hidden fixed top-0 left-0 w-screen h-screen bg-[hsl(0,0%,0%)] flex-col	justify-center	overflow-hidden	">
+        <div className="menu closed left-[-1000px] duration-[400ms] hidden fixed top-0 left-0 w-screen h-screen bg-[hsl(0,0%,0%)] flex-col	justify-center	overflow-hidden	md:flex">
           <a href="#" className="menu__link">
             About
           </a>
