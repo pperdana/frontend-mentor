264a036d6793b7ec3e9ad63b2f50c618b9950432
Panji <panji9502@gmail.com>
2023-11-07 09:05:01 +0700

    create Image component

---
+++ product-preview-card/src/Image.jsx
@@ -0,0 +1,12 @@
+const Image = () => {
+  return (
+    <img
+      src="/public/images/image-product-desktop.jpg"
+      alt="perfume chanel image"
+      width="268"
+      height="auto"
+    />
+  );
+};
+
+export default Image;
