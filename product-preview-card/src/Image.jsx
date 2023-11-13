5e1428a6d229ab64194b17a29599d83ca42e1fd9
Panji <panji9502@gmail.com>
2023-11-13 14:05:27 +0700

    img style for md screen

--- product-preview-card/src/Image.jsx
+++ product-preview-card/src/Image.jsx
@@ -1,5 +1,7 @@
 const Image = () => {
-  return <div className="bg-parfume bg-no-repeat	bg-cover	 w-[940px] h-auto" />;
+  return (
+    <div className="bg-parfume bg-no-repeat	bg-cover	 w-[940px] h-auto md:bg-parfume-mobile md:w-[360px] md:h-[240px]" />
+  );
 };
 
 export default Image;
