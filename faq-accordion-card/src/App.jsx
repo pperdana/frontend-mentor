66c0c4b3cedbbc659b29f663d68779a34d74dbfd
Panji <panji9502@gmail.com>
2023-12-05 15:36:57 +0700

    update card

--- faq-accordion-card/src/App.jsx
+++ faq-accordion-card/src/App.jsx
@@ -4,7 +4,7 @@ import Title from "./Title";
 function App() {
   return (
     <main className="min-h-screen	flex items-center	justify-center">
-      <div className="card p-[1.5rem] shadow-[0px_30px_100px_-50px_black]">
+      <div className="card p-[1.5rem] shadow-[0px_30px_100px_-50px_black] bg-[hsl(0,0%,100%)]">
         <Title />
         <Questions />
       </div>
