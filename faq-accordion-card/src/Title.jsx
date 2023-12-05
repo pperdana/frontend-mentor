64951429b5d31c5e66f91c88ab2181c2ab101218
Panji <panji9502@gmail.com>
2023-12-05 15:31:02 +0700

    update

--- faq-accordion-card/src/Title.jsx
+++ faq-accordion-card/src/Title.jsx
@@ -2,12 +2,12 @@ import star from "./assets/icon-star.svg";
 
 const Title = () => {
   return (
-    <>
+    <div className="faq-title flex">
       <img src={star} alt="star" />
       <h1>
         <span>faq</span>s
       </h1>
-    </>
+    </div>
   );
 };
 
