5b1f179b7c6a151cd2c71b88ea0b2a5469cfe715
Panji <panji9502@gmail.com>
2023-12-09 14:31:21 +0700

    add data

---
+++ faq-accordion-card/src/data.js
@@ -0,0 +1,23 @@
+const questions = [
+  {
+    id: 1,
+    title: "What is Frontend Mentor, and how will it help me?",
+    info: "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+  },
+  {
+    id: 2,
+    title: "Is Frontend Mentor free?",
+    info: "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+  },
+  {
+    id: 3,
+    title: "Can I use Frontend Mentor projects in my portfolio?",
+    info: "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+  },
+  {
+    id: 4,
+    title: "How can I get help if I'm stuck on a challenge?",
+    info: "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+  },
+];
+export default questions;
