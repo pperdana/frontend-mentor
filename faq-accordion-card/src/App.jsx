4829b958f344feb53e7b5696da6c4096f59b3ca8
Panji <panji9502@gmail.com>
2023-12-12 23:17:50 +0700

    import data

--- faq-accordion-card/src/App.jsx
+++ faq-accordion-card/src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import QuestionList from "./QuestionList";
 import Title from "./Title";
 
+import data from "./data";
+
 function App() {
+  const [questions, setQuestions] = useState(data);
+
   return (
     <main className="min-h-screen	flex items-center	justify-center">
       <div className="card p-[1.5rem] shadow-[0px_30px_100px_-50px_black] bg-[hsl(0,0%,100%)] rounded-[1rem]">
         <Title />
-        <QuestionList />
+        <QuestionList questions={questions} />
       </div>
     </main>
   );
