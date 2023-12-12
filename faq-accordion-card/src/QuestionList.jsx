dd6704d74742258c2931b516c2f48f2ad62765cf
Panji <panji9502@gmail.com>
2023-12-12 23:18:44 +0700

    display questions

--- faq-accordion-card/src/QuestionList.jsx
+++ faq-accordion-card/src/QuestionList.jsx
@@ -1,5 +1,13 @@
-const QuestionList = () => {
-  return <div>QuestionList</div>;
+import Question from "./Question";
+
+const QuestionList = ({ questions }) => {
+  return (
+    <div className="question-container">
+      {questions.map((question, index) => {
+        return <Question key={index} question={question} />;
+      })}
+    </div>
+  );
 };
 
 export default QuestionList;
