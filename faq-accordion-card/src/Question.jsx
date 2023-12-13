7ddcaa62c95b51ed44b4c899337889bf186356d5
Panji <panji9502@gmail.com>
2023-12-13 11:21:47 +0700

    add icon

--- faq-accordion-card/src/Question.jsx
+++ faq-accordion-card/src/Question.jsx
@@ -1,5 +1,24 @@
-const Question = () => {
-  return <div>Question</div>;
+import { useState } from "react";
+
+import { FaCircleMinus } from "react-icons/fa6";
+import { FaCirclePlus } from "react-icons/fa6";
+
+const Question = ({ question }) => {
+  const [showAnswer, setShowAnswer] = useState(false);
+  const { id, info, title } = question;
+
+  return (
+    <div className="text-box">
+      <div className="question">
+        <span className="text">{title}</span>
+        <FaCirclePlus
+          className="btn"
+          onClick={() => setShowAnswer(!showAnswer)}
+        />
+      </div>
+      {showAnswer && <p className="answer">{info}</p>}
+    </div>
+  );
 };
 
 export default Question;
