2aa8b7b64e2465cd5e3572ad1545018202b3c6f8
Panji <panji9502@gmail.com>
2023-12-14 12:28:24 +0700

    add padding bottom

--- faq-accordion-card/src/Title.jsx
+++ faq-accordion-card/src/Title.jsx
@@ -2,7 +2,7 @@ import star from "./assets/icon-star.svg";
 
 const Title = () => {
   return (
-    <div className="faq-title flex items-center gap-[1.5rem]">
+    <div className="faq-title flex items-center gap-[1.5rem] pb-[1rem]">
       <img src={star} alt="star" />
       <h1 className="text-[3.5rem] font-black">
         <span className="uppercase">faq</span>s
