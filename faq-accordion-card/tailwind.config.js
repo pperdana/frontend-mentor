1b87860580c92be5df711b08de1cdd96ba9c9add
Panji <panji9502@gmail.com>
2023-12-15 14:10:09 +0700

    add background image

--- faq-accordion-card/tailwind.config.js
+++ faq-accordion-card/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "desktop-pattern": "url('/src/assets/background-pattern-desktop.svg')",
+        "mobile-pattern": "url('/src/assets/background-pattern-mobile.svg')",
+      },
+    },
   },
   plugins: [],
 };
