6292ab12346490089fdbe56b0596c84cfea522ea
Panji <panji9502@gmail.com>
2023-12-20 19:35:28 +0700

    update divider

--- faq-accordion-card/src/Question.jsx
+++ faq-accordion-card/src/Question.jsx
@@ -8,32 +8,31 @@ const Question = ({ question }) => {
   const { id, info, title } = question;
 
   return (
-    <div className="text-box">
-      <div className="question p-[1rem] flex items-center justify-between gap-[2rem] text-[1.25rem] font-bold">
-        <span className="text hover:text-[hsl(281,83%,54%)] duration-300 cursor-pointer">
-          {title}
-        </span>
-        {showAnswer ? (
-          <FaCircleMinus
-            className="btn text-[25px] cursor-pointer"
-            onClick={() => setShowAnswer(!showAnswer)}
-          />
-        ) : (
-          <FaCirclePlus
-            className="btn text-[25px] text-[hsl(281,83%,54%)] cursor-pointer"
-            onClick={() => setShowAnswer(!showAnswer)}
-          />
-        )}
+    <>
+      <div className="text-box p-[1rem]">
+        <div className="question p-[1rem] flex items-center justify-between gap-[2rem] text-[1.25rem] font-bold">
+          <span className="text duration-300">{title}</span>
+          {showAnswer ? (
+            <FaCircleMinus
+              className="btn text-[25px] cursor-pointer"
+              onClick={() => setShowAnswer(!showAnswer)}
+            />
+          ) : (
+            <FaCirclePlus
+              className="btn text-[25px] text-[hsl(281,83%,54%)] cursor-pointer"
+              onClick={() => setShowAnswer(!showAnswer)}
+            />
+          )}
+        </div>
+
+        {showAnswer && <p className="answer p-[1rem] max-w-[60ch]">{info}</p>}
       </div>
-
-      {showAnswer && <p className="answer p-[1rem] max-w-[60ch]">{info}</p>}
-
       {id !== 4 ? (
         <div className="divider border-b-[1px] border-solid	border-[hsla(292,16%,49%,.2)]" />
       ) : (
         <div className="hidden" />
       )}
-    </div>
+    </>
   );
 };
 
