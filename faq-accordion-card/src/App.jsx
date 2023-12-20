46863458521a6eb25ba971c26b5d1e9455d37bfe
Panji <panji9502@gmail.com>
2023-12-20 19:35:52 +0700

    add vertical margin

--- faq-accordion-card/src/App.jsx
+++ faq-accordion-card/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <main className="min-h-screen	flex items-center	justify-center">
-      <div className="card p-[1.5rem] max-w-[1000px] min-h-[400px] shadow-[0px_30px_100px_-50px_black] bg-[hsl(0,0%,100%)] rounded-[1rem]">
+      <div className="card my-[3rem] p-[1.5rem] max-w-[1000px] min-h-[400px] shadow-[0px_30px_100px_-50px_black] bg-[hsl(0,0%,100%)] rounded-[1rem]">
         <Title />
         <QuestionList questions={questions} />
       </div>
