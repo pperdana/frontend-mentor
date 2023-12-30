85a10fd5ec2f74d331c0951ae710c99b422da04e
Panji <panji9502@gmail.com>
2023-12-30 20:26:08 +0700

    add components

--- ping-comming-soon/src/App.jsx
+++ ping-comming-soon/src/App.jsx
@@ -1,5 +1,13 @@
+import Ping from "./Ping";
+import Header from "./Header";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Header />
+      <Ping />
+    </>
+  );
 }
 
 export default App;
