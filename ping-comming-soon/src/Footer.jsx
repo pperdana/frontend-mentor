eb15f0241eea5f49f708c92e4a51a3b8c5cd94ff
Panji <panji9502@gmail.com>
2024-01-03 09:32:48 +0700

    add logo

--- ping-comming-soon/src/Footer.jsx
+++ ping-comming-soon/src/Footer.jsx
@@ -1,8 +1,22 @@
+import { FaFacebookF } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa6";
+
 const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <div className="logo">logo</div>
+        <div className="logo">
+          <div className="fb">
+            <FaFacebookF />
+          </div>
+          <div className="tw">
+            <FaTwitter />
+          </div>
+          <div className="ig">
+            <FaInstagram />
+          </div>
+        </div>
         <div className="copyright">
           <p>© Copyright Ping. All rights reserved.</p>
         </div>
