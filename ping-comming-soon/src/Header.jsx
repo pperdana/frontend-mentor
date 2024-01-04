76d2e55448f4abf6468adfd4b6bd0f0383010559
Panji <panji9502@gmail.com>
2024-01-04 14:54:19 +0700

    style logo

--- ping-comming-soon/src/Header.jsx
+++ ping-comming-soon/src/Header.jsx
@@ -2,7 +2,7 @@ import logo from "./assets/logo.svg";
 
 const Header = () => {
   return (
-    <header>
+    <header className="flex justify-center my-[5.6rem]">
       <img src={logo} alt="logo" />
     </header>
   );
