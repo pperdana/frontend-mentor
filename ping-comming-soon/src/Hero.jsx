340bdc8f57e803c65eb7634daee48b6aa41f0110
Panji <panji9502@gmail.com>
2024-01-05 14:41:53 +0700

    style form

--- ping-comming-soon/src/Hero.jsx
+++ ping-comming-soon/src/Hero.jsx
@@ -4,15 +4,23 @@ const Hero = () => {
   return (
     <main>
       <section className="form text-center">
-        <h1 className="text-[3rem]">
+        <h1 className="text-[3rem] mb-[1.15rem]">
           We are launching <strong>soon!</strong>
         </h1>
-        <p className="subscribe text-[1.25rem]">Subscribe and get notified</p>
-        <form id="form" action="/" method="post">
+        <p className="subscribe text-[1.25rem] mb-[2.28rem]">
+          Subscribe and get notified
+        </p>
+        <form
+          id="form"
+          className="w-[60%] my-0  mb-auto flex flex-wrap	justify-center items-center gap-[0.5rem]"
+          action="/"
+          method="post"
+        >
           <input
             type="email"
             name="email"
             id="email"
+            className="rounded-[2rem]"
             placeholder="Your email address..."
           />
           <button type="submit" id="button">
