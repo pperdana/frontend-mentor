47b1cc3e35019d4da401d8012f58ffef873023ae
Panji <panji9502@gmail.com>
2024-01-09 09:38:20 +0700

    update icon and icon-container class

--- ping-comming-soon/src/Footer.jsx
+++ ping-comming-soon/src/Footer.jsx
@@ -3,16 +3,16 @@ import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa6";
 const Footer = () => {
   return (
     <footer>
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center mt-[5rem]">
         <div className="logo flex gap-[1rem]">
-          <div className="fb icon">
-            <FaFacebookF />
+          <div className="fb icon-container">
+            <FaFacebookF className="icon" />
           </div>
-          <div className="tw icon">
-            <FaTwitter />
+          <div className="tw icon-container">
+            <FaTwitter className="icon" />
           </div>
-          <div className="ig icon">
-            <FaInstagram />
+          <div className="ig icon-container">
+            <FaInstagram className="icon" />
           </div>
         </div>
         <div className="copyright">
