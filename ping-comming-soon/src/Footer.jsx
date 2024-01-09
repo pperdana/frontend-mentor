3e2946063f0373fdc233b816f2cf8f487d90cf83
Panji <panji9502@gmail.com>
2024-01-09 09:31:55 +0700

    add icon style

--- ping-comming-soon/src/Footer.jsx
+++ ping-comming-soon/src/Footer.jsx
@@ -3,15 +3,15 @@ import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa6";
 const Footer = () => {
   return (
     <footer>
-      <div className="container flex flex-col justify-center items-center">
-        <div className="logo flex">
-          <div className="fb">
+      <div className="flex flex-col justify-center items-center">
+        <div className="logo flex gap-[1rem]">
+          <div className="fb icon">
             <FaFacebookF />
           </div>
-          <div className="tw">
+          <div className="tw icon">
             <FaTwitter />
           </div>
-          <div className="ig">
+          <div className="ig icon">
             <FaInstagram />
           </div>
         </div>
