f180182fbb9a2061ed8591c7be6e0467a6a4bbd6
Panji <panji9502@gmail.com>
2024-01-11 12:06:40 +0700

    add medium screen size

--- ping-comming-soon/tailwind.config.js
+++ ping-comming-soon/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      md: { max: "39em" },
+    },
   },
   plugins: [],
 };
