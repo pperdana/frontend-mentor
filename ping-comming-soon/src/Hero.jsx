10919e822db95e94e811183f71fc6b7ef0685e55
Panji <panji9502@gmail.com>
2024-01-15 11:08:54 +0700

    add form-box

--- ping-comming-soon/src/Hero.jsx
+++ ping-comming-soon/src/Hero.jsx
@@ -24,31 +24,34 @@ const Hero = () => {
         <p className="subscribe text-[1.25rem] mb-[2.28rem] sm:text-[0.85rem]">
           Subscribe and get notified
         </p>
-        <form
-          id="form"
-          className="my-0  mb-auto relative flex flex-wrap	justify-center items-center gap-[0.5rem]"
-          onSubmit={handleSubmit}
-        >
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="h-[55px] rounded-[2rem] max-w-[350px] m-0 text-[1.125rem]  w-[100%] outline-none	border-[1px] border-solid border-[hsl(223,100%,88%)] py-[16px] px-[32px] md:max-w-[330px] sm:max-w-[80%] sm:h-[48px] sm:text-[1rem]"
-            placeholder="Your email address..."
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <p className="absolute text-[12px] top-[135%]	left-[37%] translate-x-[-50%] translate-y-[-50%]">
-            please provide valid email address
-          </p>
-          <button
-            type="submit"
-            id="button"
-            className="h-[55px] w-[170px] bg-[hsl(223,87%,63%)]  text-white	cursor-pointer shadow-[2px_3px_10px_rgba(79,125,242,.5)] transition-all	duration-300 ease-linear rounded-[2rem] text-[18px] hover:bg-[rgba(79,125,242,.8)] hover:shadow-[2px_3px_10px_rgba(79,125,242,.3)] sm:w-[80%] sm:h-[48px]"
+
+        <div className="form-box flex items-center">
+          <form
+            id="form"
+            className="my-0  mb-auto relative flex items-center items-center"
+            onSubmit={handleSubmit}
           >
-            Notify Me
-          </button>
-        </form>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="h-[55px] rounded-[2rem] max-w-[350px] m-0 text-[1.125rem]  w-[100%] outline-none	border-[1px] border-solid border-[hsl(223,100%,88%)] py-[16px] px-[32px] md:max-w-[330px] sm:max-w-[80%] sm:h-[48px] sm:text-[1rem]"
+              placeholder="Your email address..."
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <p className="text-[12px] absolute top-[50%]	left-[50%] translate-x-[-50%] translate-y-[-50%]">
+              please provide valid email address
+            </p>
+            <button
+              type="submit"
+              id="button"
+              className="h-[55px] w-[170px] bg-[hsl(223,87%,63%)]  text-white	cursor-pointer shadow-[2px_3px_10px_rgba(79,125,242,.5)] transition-all	duration-300 ease-linear rounded-[2rem] text-[18px] hover:bg-[rgba(79,125,242,.8)] hover:shadow-[2px_3px_10px_rgba(79,125,242,.3)] sm:w-[80%] sm:h-[48px]"
+            >
+              Notify Me
+            </button>
+          </form>
+        </div>
       </section>
 
       <section className="illustration flex justify-center mt-[5rem]">
