2823faf85b97021333df4c96511e30b87aaa42dd
Panji <panji9502@gmail.com>
2024-01-18 11:10:46 +0700

    update content

--- blog-preview-card/src/App.jsx
+++ blog-preview-card/src/App.jsx
@@ -5,30 +5,28 @@ const App = () => {
         <div className="card-img">
           <img src="/assets/images/illustration-article.svg" alt="card image" />
         </div>
-        <div className="card-tittle">
-          <p>Learning</p>
-        </div>
-        <div className="card-date">
-          <p>Published 21 Dec 2023</p>
-        </div>
-        <div className="card-subtittle">
-          <h1>HTML &amp; CSS foundations</h1>
-        </div>
-        <div className="card-text">
-          <p>
+
+        <div className="card-info">
+          <p className="card-type">Learning</p>
+          <p className="card-date">Published 21 Dec 2023</p>
+
+          <h1 className="card-title">HTML &amp; CSS foundations</h1>
+
+          <p className="card-desc">
             These languages are the backbone of every website, defining
             structure, content, and presentation.
           </p>
-        </div>
-        <div className="card-profile">
-          <div className="avatar">
-            <img
-              src="/assets/images/image-avatar.webp"
-              alt="avatar"
-              className="avatar-img"
-            />
+
+          <div className="card-profile">
+            <div className="avatar">
+              <img
+                src="/assets/images/image-avatar.webp"
+                alt="avatar"
+                className="avatar-img"
+              />
+            </div>
+            <p className="avatar-name">Greg Hooper</p>
           </div>
-          <p className="avatar-name">Greg Hooper</p>
         </div>
       </div>
     </>
