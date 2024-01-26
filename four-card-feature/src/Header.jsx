c7e504a376f26da3e39551a379d87677e1e120f1
Panji <panji9502@gmail.com>
2024-01-26 22:32:52 +0700

    create header component

---
+++ four-card-feature/src/Header.jsx
@@ -0,0 +1,18 @@
+const Header = () => {
+  return (
+    <header>
+      <p className="header__text header__text--weight-400 header__text--medium-font">
+        Reliable, efficient delivery
+      </p>
+      <h1 className="header__text header__text--weight-600 header__text--medium-font">
+        Powered by Technology
+      </h1>
+      <p className="header__text header__text--weight-200">
+        Our Artificial intelligence powered tools use millions of project data
+        points to ensure that your project is successful
+      </p>
+    </header>
+  );
+};
+
+export default Header;
