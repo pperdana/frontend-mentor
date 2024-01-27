9f2501abf2374fad238ff27e7aa4c70ee9230876
Panji <panji9502@gmail.com>
2024-01-27 11:04:24 +0700

    style header text

--- four-card-feature/src/Header.jsx
+++ four-card-feature/src/Header.jsx
@@ -1,13 +1,11 @@
 const Header = () => {
   return (
     <header>
-      <p className="header__text header__text--weight-400 header__text--medium-font">
+      <p className="about text-center text-[40px]">
         Reliable, efficient delivery
       </p>
-      <h1 className="header__text header__text--weight-600 header__text--medium-font">
-        Powered by Technology
-      </h1>
-      <p className="header__text header__text--weight-200">
+      <h1 className="title text-center text-[40px]">Powered by Technology</h1>
+      <p className="desc text-center text-[15px]">
         Our Artificial intelligence powered tools use millions of project data
         points to ensure that your project is successful
       </p>
