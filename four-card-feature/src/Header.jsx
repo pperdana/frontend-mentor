2d684c6dc763686209bbcc409bb7959a80fb9195
Panji <panji9502@gmail.com>
2024-01-30 11:45:01 +0700

    add font weight

--- four-card-feature/src/Header.jsx
+++ four-card-feature/src/Header.jsx
@@ -1,11 +1,13 @@
 const Header = () => {
   return (
     <header>
-      <p className="about text-center text-[40px]">
+      <p className="about text-center text-[45px]">
         Reliable, efficient delivery
       </p>
-      <h1 className="title text-center text-[40px]">Powered by Technology</h1>
-      <p className="desc text-center text-[15px]">
+      <h1 className="title text-center text-[45px] font-semibold">
+        Powered by Technology
+      </h1>
+      <p className="desc text-center text-[15px] font-extralight">
         Our Artificial intelligence powered tools use millions of project data
         points to ensure that your project is successful
       </p>
