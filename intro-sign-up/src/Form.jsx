25f797e680e99d5c3bf7c31a12381e31e6c0bb9a
Panji <panji9502@gmail.com>
2024-02-03 11:41:16 +0700

    Create form component

---
+++ intro-sign-up/src/Form.jsx
@@ -0,0 +1,55 @@
+const Form = () => {
+  return (
+    <div className="form-container">
+      <div className="header">
+        Try it free 7 days
+        <span>then $20/mo. thereafter</span>
+      </div>
+      <form id="fill">
+        <div className="fill-item">
+          <input
+            type="text"
+            name="firstName"
+            id="firstname"
+            placeholder="First Name"
+          />
+          <div className="error-text"></div>
+        </div>
+        <div className="fill-item">
+          <input
+            type="text"
+            name="lastName"
+            id="lastname"
+            placeholder="Last Name"
+          />
+          <div className="error-text"></div>
+        </div>
+        <div className="fill-item">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email Address"
+          />
+          <div className="error-text"></div>
+        </div>
+        <div className="fill-item">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+          <div className="error-text"></div>
+        </div>
+        <input id="submit" type="button" value="CLAIM YOUR FREE TRIAL" />
+        <p>
+          By clicking the button, you are agreeing to our{" "}
+          <span>Terms and Services</span>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
