4a7a2ccec75685d71dcb16393695e113ac2e8771
Panji <panji9502@gmail.com>
2024-02-05 10:02:57 +0700

    Update style

--- intro-sign-up/src/Title.jsx
+++ intro-sign-up/src/Title.jsx
@@ -1,8 +1,10 @@
 const Title = () => {
   return (
     <div className="title-container">
-      <h1>Learn to code by watching others</h1>
-      <p>
+      <h1 className="text-[2.5rem] text-start">
+        Learn to code by watching others
+      </h1>
+      <p className="text-[1rem] leading-[25px] text-start">
         See how experienced developers solve problems in real-time. Watching
         scripted tutorials is great, but understanding how developers think is
         invaluable.
