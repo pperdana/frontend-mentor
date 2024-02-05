f91587fc612c034e5178a09f895b552e97770bd3
Panji <panji9502@gmail.com>
2024-02-05 10:29:56 +0700

    form style

--- intro-sign-up/src/Form.jsx
+++ intro-sign-up/src/Form.jsx
@@ -3,7 +3,7 @@ const Form = () => {
     <div className="form-container flex flex-col gap-[20px]">
       <div className="header bg-[hsl(248,32%,49%)] py-[15px] px-[30px] text-[0.9rem] font-bold	rounded-[10px] shadow-[4px_5px_8px_4px_rgba(62,60,73,0.342)] transition-all	duration-500">
         Try it free 7 days
-        <span>then $20/mo. thereafter</span>
+        <span className="font-normal">then $20/mo. thereafter</span>
       </div>
 
       <form
@@ -12,6 +12,7 @@ const Form = () => {
       >
         <div className="fill-item">
           <input
+            className="form-input"
             type="text"
             name="firstName"
             id="firstname"
@@ -21,6 +22,7 @@ const Form = () => {
         </div>
         <div className="fill-item">
           <input
+            className="form-input"
             type="text"
             name="lastName"
             id="lastname"
@@ -30,6 +32,7 @@ const Form = () => {
         </div>
         <div className="fill-item">
           <input
+            className="form-input"
             type="email"
             name="email"
             id="email"
@@ -39,6 +42,7 @@ const Form = () => {
         </div>
         <div className="fill-item">
           <input
+            className="form-input"
             type="password"
             name="password"
             id="password"
@@ -46,10 +50,15 @@ const Form = () => {
           />
           <div className="error-text"></div>
         </div>
-        <input id="submit" type="button" value="CLAIM YOUR FREE TRIAL" />
-        <p>
-          By clicking the button, you are agreeing to our{" "}
-          <span>Terms and Services</span>
+        <input
+          id="submit"
+          className="border-none	bg-[hsl(154,59%,51%)] rounded-[5px] shadow-[4px_5px_8px_4px_rgba(62,60,73,0.342)] text-white text-[1rem] py-[15px] px-[20px] font-bold	cursor-pointer	transition-all	duration-500"
+          type="button"
+          value="CLAIM YOUR FREE TRIAL"
+        />
+        <p className="text-[hsl(246,25%,77%)] text-[0.85rem]">
+          By clicking the button, you are agreeing to our
+          <span className="text-[hsl(0,100%,74%)]">Terms and Services</span>
         </p>
       </form>
     </div>
