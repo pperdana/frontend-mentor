d55fa988b08d65782dd8036a2a02888dedc16fe6
Panji <panji9502@gmail.com>
2024-02-09 21:45:11 +0700

    update sm styles

--- intro-sign-up/src/Title.jsx
+++ intro-sign-up/src/Title.jsx
@@ -1,10 +1,10 @@
 const Title = () => {
   return (
     <div className="title-container sm:flex sm:flex-col sm:items-center">
-      <h1 className="text-[2.5rem] text-start font-bold leading-[50px] md:text-center md:mb-[2.6rem] sm:w-[350px]">
+      <h1 className="text-[2.5rem] text-start font-bold leading-[50px] md:text-center md:mb-[2.6rem] sm:mx-[1.8rem] sm:text-[1.8rem] sm:mb-[1.2rem] sm:leading-[40px]">
         Learn to code by watching others
       </h1>
-      <p className="text-[14px] leading-[23px] text-start text-gray-100 mt-[1.4rem] md:text-center sm:mx-[2rem] sm:text-[20px]">
+      <p className="text-[14px] leading-[23px] text-start text-gray-100 mt-[1.4rem] md:text-center sm:mx-[1.6rem] sm:text-[16px] sm:mt-0 sm:leading-[26px]">
         See how experienced developers solve problems in real-time. Watching
         scripted tutorials is great, but understanding how developers think is
         invaluable.
