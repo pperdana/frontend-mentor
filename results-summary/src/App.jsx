002ade0ce932523406cc3d49d9f53155bf0f9dc5
Panji <panji9502@gmail.com>
2024-02-16 07:59:50 +0700

    Create summary component

--- results-summary/src/App.jsx
+++ results-summary/src/App.jsx
@@ -1,59 +1,12 @@
+import Summary from "./Summary";
 import Card from "./Card";
+
 const App = () => {
   return (
     <main>
       <div className="section">
         <Card />
-
-        <div className="summary">
-          <h1>Summary</h1>
-
-          <div className="score">
-            <section className="icon">
-              <img src="assets/images/icon-reaction.svg" alt="icon-reaction" />
-              <p className="reaction">Reaction</p>
-            </section>
-            <section className="result">
-              <strong>80</strong>
-              <p>/&nbsp;100</p>
-            </section>
-          </div>
-
-          <div className="score">
-            <section className="icon">
-              <img src="assets/images/icon-memory.svg" alt="icon-memory" />
-              <p className="memory">Memory</p>
-            </section>
-            <section className="result">
-              <strong>80</strong>
-              <p>/&nbsp;100</p>
-            </section>
-          </div>
-
-          <div className="score">
-            <section className="icon">
-              <img src="assets/images/icon-verbal.svg" alt="icon-verbal" />
-              <p className="verbal">Verbal</p>
-            </section>
-            <section className="result">
-              <strong>80</strong>
-              <p>/&nbsp;100</p>
-            </section>
-          </div>
-
-          <div className="score">
-            <section className="icon">
-              <img src="assets/images/icon-visual.svg" alt="icon-visual" />
-              <p className="visual">Visual</p>
-            </section>
-            <section className="result">
-              <strong>80</strong>
-              <p>/&nbsp;100</p>
-            </section>
-          </div>
-
-          <a href="#">Continue</a>
-        </div>
+        <Summary />
       </div>
     </main>
   );
