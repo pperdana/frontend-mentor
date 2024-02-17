e921a27943ac94042e38d7f23ea70511e8ebd2ae
Panji <panji9502@gmail.com>
2024-02-17 11:50:59 +0700

    add data

---
+++ results-summary/src/data.jsx
@@ -0,0 +1,32 @@
+const data = [
+  {
+    category: "Reaction",
+    score: 80,
+    icon: "src/assets/icon-reaction.svg",
+    alt: "icon-reaction",
+    iconColor: "hsl(0,100%,67%)",
+  },
+  {
+    category: "Memory",
+    score: 92,
+    icon: "src/assets/icon-memory.svg",
+    alt: "icon-memory",
+    iconColor: "hsl(39,100%,56%)",
+  },
+  {
+    category: "Verbal",
+    score: 61,
+    icon: "src/assets/icon-verbal.svg",
+    alt: "icon-verbal",
+    iconColor: "hsl(166,100%,37%)",
+  },
+  {
+    category: "Visual",
+    score: 72,
+    icon: "src/assets/icon-visual.svg",
+    alt: "icon-visual",
+    iconColor: "hsl(234,85%,45%)",
+  },
+];
+
+export default data;
