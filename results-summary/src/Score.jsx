aaa41b0394d5d29672a4fcfb15c6daf08c0278bf
Panji <panji9502@gmail.com>
2024-02-17 11:50:48 +0700

    Create score component

---
+++ results-summary/src/Score.jsx
@@ -0,0 +1,20 @@
+const Score = ({ scores }) => {
+  const { category, score, icon, alt, iconColor } = scores;
+
+  console.log(scores);
+
+  return (
+    <div className="score">
+      <section className="icon">
+        <img src={icon} alt={alt} />
+        <p className={`reaction text-[${iconColor}]`}>{category}</p>
+      </section>
+      <section className="result">
+        <strong className="font-bold">{score}</strong>
+        <p>/&nbsp;100</p>
+      </section>
+    </div>
+  );
+};
+
+export default Score;
--- results-summary/src/Summary.jsx
+++ results-summary/src/Summary.jsx
@@ -1,58 +1,17 @@
-import iconMemory from "./assets/icon-memory.svg";
-import iconReaction from "./assets/icon-reaction.svg";
-import iconVerbal from "./assets/icon-verbal.svg";
-import iconVisual from "./assets/icon-visual.svg";
+import Score from "./Score";
+import data from "./data";
 
 const Summary = () => {
+  console.log(data);
   return (
     <div className="summary">
       <h1 className="text-[hsl(224,30%,27%)] text-[1.5rem] mb-[2rem] font-bold">
         Summary
       </h1>
 
-      <div className="score">
-        <section className="icon">
-          <img src={iconReaction} alt="icon-reaction" />
-          <p className="reaction text-[hsl(0,100%,67%)]">Reaction</p>
-        </section>
-        <section className="result">
-          <strong>80</strong>
-          <p>/&nbsp;100</p>
-        </section>
-      </div>
-
-      <div className="score">
-        <section className="icon">
-          <img src={iconMemory} alt="icon-memory" />
-          <p className="memory text-[hsl(39,100%,56%)]">Memory</p>
-        </section>
-        <section className="result">
-          <strong>80</strong>
-          <p>/&nbsp;100</p>
-        </section>
-      </div>
-
-      <div className="score">
-        <section className="icon">
-          <img src={iconVerbal} alt="icon-verbal" />
-          <p className="verbal text-[hsl(166,100%,37%)]">Verbal</p>
-        </section>
-        <section className="result">
-          <strong>80</strong>
-          <p>/&nbsp;100</p>
-        </section>
-      </div>
-
-      <div className="score">
-        <section className="icon">
-          <img src={iconVisual} alt="icon-visual" />
-          <p className="visual text-[hsl(234,85%,45%)]">Visual</p>
-        </section>
-        <section className="result">
-          <strong>80</strong>
-          <p>/&nbsp;100</p>
-        </section>
-      </div>
+      {data.map((scores) => {
+        return <Score scores={scores} key={scores.category} />;
+      })}
 
       <button className="btn">Continue</button>
     </div>
