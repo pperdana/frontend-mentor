85e7ba718fcf8108e51593d366e0d43f5d783b8f
Panji <panji9502@gmail.com>
2024-02-19 10:50:39 +0700

    Add content

--- social-links-profile/src/App.jsx
+++ social-links-profile/src/App.jsx
@@ -1,5 +1,24 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="container">
+      <div className="first">
+        <img src="assets/images/avatar-jessica.jpeg" />
+        <div className="semiFirst">
+          <div className="bolded">Jessica Randall</div>
+          <div className="yellow">London, United Kingdom</div>
+        </div>
+      </div>
+
+      <div className="second">
+        <p>Front-end developer and avid reader.</p>
+      </div>
+      <button>Github</button>
+      <button>Frontend Mentor</button>
+      <button>LinkedIn</button>
+      <button>Twitter</button>
+      <button>Instagram</button>
+    </main>
+  );
 }
 
 export default App;
