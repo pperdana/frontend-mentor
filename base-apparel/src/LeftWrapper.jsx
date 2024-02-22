eb2aa5e28dcd0111fddfa4f48e28823b08c6fe17
Panji <panji9502@gmail.com>
2024-02-22 11:34:09 +0700

    Update LeftWrapper styles

--- base-apparel/src/LeftWrapper.jsx
+++ base-apparel/src/LeftWrapper.jsx
@@ -3,7 +3,7 @@ import arrowIcon from "./assets/icon-arrow.svg";
 
 const LeftWrapper = () => {
   return (
-    <section className="left-wrapper max-w-[50%]">
+    <section className="left-wrapper max-w-[50%] bg-[url('./assets/bg-pattern-desktop.svg')]">
       <div className="left-content-wrapper">
         <img className="w-[158px] h-[33px] mt-[3rem]" src={logo} alt="logo" />
         <section>
@@ -11,13 +11,13 @@ const LeftWrapper = () => {
         </section>
         <section className="text-wrapper">
           <div>
-            <h1 className="text-[#ce9797] text-[5rem] font-extralight">
+            <h1 className="text-[#ce9797] text-[6rem] font-extralight">
               W E ' R E
             </h1>
-            <h1 className="text-[5rem] leading-[1]">C O M I N G</h1>
-            <h1 className="text-[5rem] leading-[1]"> S O O N</h1>
+            <h1 className="text-[6rem] leading-[1]">C O M I N G</h1>
+            <h1 className="text-[6rem] leading-[1]"> S O O N</h1>
           </div>
-          <p className="text-[#ce9797] text-[1.02rem] leading-[1.7]">
+          <p className="text-[#ce9797] text-[1.5rem] leading-[1.7]">
             Hello fellow shoppers! We're currently building our new fashion
             store. Add your email below to stay up-to-date with announcements
             and our launch deals.
