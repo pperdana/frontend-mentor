434fad7f8df617b414272e61c34c7e5c5f233eb8
Panji <panji9502@gmail.com>
2024-02-24 12:35:02 +0700

    Create Card and Submitted components

---
+++ interactive-rating-component/src/Card.jsx
@@ -0,0 +1,24 @@
+const Card = () => {
+  return (
+    <section className="card-section">
+      <div className="star-icon">
+        <img src="./images/icon-star.svg" alt="Icon Star" />
+      </div>
+      <h1 className="title">How did we do?</h1>
+      <p className="text">
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </p>
+      <div className="rate">
+        <div className="rate-num">1</div>
+        <div className="rate-num">2</div>
+        <div className="rate-num">3</div>
+        <div className="rate-num">4</div>
+        <div className="rate-num">5</div>
+      </div>
+      <button className="btn">SUBMIT</button>
+    </section>
+  );
+};
+
+export default Card;
---
+++ interactive-rating-component/src/Submitted.jsx
@@ -0,0 +1,19 @@
+const Submitted = () => {
+  return (
+    <section className="submitted-section">
+      <div className="thank-you-img">
+        <img src="./images/illustration-thank-you.svg" alt="Thank You Image" />
+      </div>
+      <div className="rate-selected">
+        <p className="rate-text"></p>
+      </div>
+      <h1 className="title">Thank you!</h1>
+      <p className="text">
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don&rsquo;t hesitate to get in touch!
+      </p>
+    </section>
+  );
+};
+
+export default Submitted;
