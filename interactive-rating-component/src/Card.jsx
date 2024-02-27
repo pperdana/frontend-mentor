aee48bf1e1440c1390d13b4af6f5e77eb23280ed
Panji <panji9502@gmail.com>
2024-02-27 14:10:55 +0700

    rename class

--- interactive-rating-component/src/Card.jsx
+++ interactive-rating-component/src/Card.jsx
@@ -11,7 +11,7 @@ const Card = () => {
         />
       </div>
       <h1 className="title">How did we do?</h1>
-      <p className="card-text">
+      <p className="text">
         Please let us know how we did with your support request. All feedback is
         appreciated to help us improve our offering!
       </p>
