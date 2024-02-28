72674eea6d1bac75a761c515c7dc5c1a6ec9f2fb
Panji <panji9502@gmail.com>
2024-02-28 10:51:26 +0700

    import data

--- interactive-rating-component/src/App.jsx
+++ interactive-rating-component/src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 import Submitted from "./Submitted";
 import Card from "./Card";
+import data from "./data";
 
 const App = () => {
+  const [listNum, setListNum] = useState(data);
+
   return (
     <main className="main">
-      <Card />
+      <Card listNum={listNum} setListNum={setListNum} />
       <Submitted />
     </main>
   );
