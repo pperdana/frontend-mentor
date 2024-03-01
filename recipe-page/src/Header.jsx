d0b56087b4b13af057071a34cee914471240b5db
Panji <panji9502@gmail.com>
2024-03-01 10:31:20 +0700

    style img

--- recipe-page/src/Header.jsx
+++ recipe-page/src/Header.jsx
@@ -2,6 +2,7 @@ const Header = () => {
   return (
     <header>
       <img
+        className="block w-full"
         src="./assets/images/image-omelette.jpeg"
         alt="presentation of an omelette"
       />
