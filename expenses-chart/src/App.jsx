9bd0ec32deb9fd9e775190c32b207c751fc78d0f
Panji <panji9502@gmail.com>
2024-03-07 14:30:01 +0700

    Update content

--- expenses-chart/src/App.jsx
+++ expenses-chart/src/App.jsx
@@ -12,37 +12,37 @@ export default function App() {
         <div className="graphic_container">
           <div className="bar-container">
             <span className="bar_value">$17.45</span>
-            <div className="bar" style="height:50px;"></div>
+            <div className="bar"></div>
             <span className="bar_subtitle">mon</span>
           </div>
           <div className="bar-container">
             <span className="bar_value">$34.91</span>
-            <div className="bar" style="height:100px;"></div>
+            <div className="bar"></div>
             <span className="bar_subtitle">tue</span>
           </div>
           <div className="bar-container">
             <span className="bar_value">$52.36</span>
-            <div className="bar" style="height:150px;"></div>
+            <div className="bar"></div>
             <span className="bar_subtitle">wed</span>
           </div>
           <div className="bar-container">
             <span className="bar_value">$31.07</span>
-            <div className="bar" style="height:89px;"></div>
+            <div className="bar"></div>
             <span className="bar_subtitle">thu</span>
           </div>
           <div className="bar-container">
             <span className="bar_value">$23.39</span>
-            <div className="bar" style="height:67px;"></div>
+            <div className="bar"></div>
             <span className="bar_subtitle">fri</span>
           </div>
           <div className="bar-container">
             <span className="bar_value">$43.28</span>
-            <div className="bar" style="height:124px;"></div>
+            <div className="bar"></div>
             <span className="bar_subtitle">sat</span>
           </div>
           <div className="bar-container">
             <span className="bar_value">$25.48</span>
-            <div className="bar" style="height:73px;"></div>
+            <div className="bar"></div>
             <span className="bar_subtitle">sun</span>
           </div>
         </div>
