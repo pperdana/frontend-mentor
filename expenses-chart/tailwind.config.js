93769716fbaf037a39f64fed7eb1c54ab04976fd
Panji <panji9502@gmail.com>
2024-03-07 14:15:32 +0700

    Starter files

--- expenses-chart/tailwind.config.js
+++ expenses-chart/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      xl: "62.5%",
+      base: "56.25%",
+      sm: "50%",
+    },
+    fontFamily: {
+      // poppins: ["Poppins", "sans-serif"],
+    },
     extend: {},
+    screens: {
+      sm: { max: "28em" },
+    },
   },
   plugins: [],
 };
