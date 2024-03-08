d007770acf8198f8384f91650803a4b950af1c47
Panji <panji9502@gmail.com>
2024-03-08 19:25:22 +0700

    Import components

--- expenses-chart/src/App.jsx
+++ expenses-chart/src/App.jsx
@@ -1,62 +1,10 @@
+import { Graphic } from "./Graphic";
+import { Balance } from "./Balance";
 export default function App() {
   return (
     <main>
-      <section className="balance">
-        <h2 className="balance_caption">My balance</h2>
-        <span className="balance_value">$921.48</span>
-        <img src="images/logo.svg" alt="Logo" className="logo" />
-      </section>
-
-      <section className="spending-graphic">
-        <h1>Spending - Last 7 days</h1>
-        <div className="graphic_container">
-          <div className="bar-container">
-            <span className="bar_value">$17.45</span>
-            <div className="bar"></div>
-            <span className="bar_subtitle">mon</span>
-          </div>
-          <div className="bar-container">
-            <span className="bar_value">$34.91</span>
-            <div className="bar"></div>
-            <span className="bar_subtitle">tue</span>
-          </div>
-          <div className="bar-container">
-            <span className="bar_value">$52.36</span>
-            <div className="bar"></div>
-            <span className="bar_subtitle">wed</span>
-          </div>
-          <div className="bar-container">
-            <span className="bar_value">$31.07</span>
-            <div className="bar"></div>
-            <span className="bar_subtitle">thu</span>
-          </div>
-          <div className="bar-container">
-            <span className="bar_value">$23.39</span>
-            <div className="bar"></div>
-            <span className="bar_subtitle">fri</span>
-          </div>
-          <div className="bar-container">
-            <span className="bar_value">$43.28</span>
-            <div className="bar"></div>
-            <span className="bar_subtitle">sat</span>
-          </div>
-          <div className="bar-container">
-            <span className="bar_value">$25.48</span>
-            <div className="bar"></div>
-            <span className="bar_subtitle">sun</span>
-          </div>
-        </div>
-        <div className="graphic_info">
-          <div className="graphic_info-left">
-            <h2 className="month_caption">Total this month</h2>
-            <span className="month_value"> $478.33</span>
-          </div>
-          <div className="graphic_info-rigth">
-            <span className="percentage_value">+2.4%</span>
-            <p className="percentage_paragraph"> from last month</p>
-          </div>
-        </div>
-      </section>
+      <Balance />
+      <Graphic />
     </main>
   );
 }
---
+++ expenses-chart/src/Balance.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+export function Balance({}) {
+  return (
+    <section className="balance">
+      <h2 className="balance_caption">My balance</h2>
+      <span className="balance_value">$921.48</span>
+      <img src="images/logo.svg" alt="Logo" className="logo" />
+    </section>
+  );
+}
