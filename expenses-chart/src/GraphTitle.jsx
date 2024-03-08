5b0115ba924d78ea84d8884a292124a586dd0f4f
Panji <panji9502@gmail.com>
2024-03-08 19:41:32 +0700

    Create GraphTitle component

---
+++ expenses-chart/src/GraphTitle.jsx
@@ -0,0 +1,5 @@
+const GraphTitle = () => {
+  return <h1>Spending - Last 7 days</h1>;
+};
+
+export default GraphTitle;
--- expenses-chart/src/Graphic.jsx
+++ expenses-chart/src/Graphic.jsx
@@ -1,10 +1,11 @@
+import GraphTitle from "./GraphTitle";
 import GraphInfo from "./GraphInfo";
 import GraphContainer from "./GraphContainer";
 
 const Graphic = () => {
   return (
     <section className="spending-graphic">
-      <h1>Spending - Last 7 days</h1>
+      <GraphTitle />
       <GraphContainer />
       <GraphInfo />
     </section>
