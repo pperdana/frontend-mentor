3e6d43a159bb573daa04fb2d98c0f79bd19e32a4
Panji <panji9502@gmail.com>
2024-03-09 15:35:05 +0700

    Import logo

--- expenses-chart/src/Balance.jsx
+++ expenses-chart/src/Balance.jsx
@@ -1,9 +1,11 @@
+import logo from "./assets/logo.svg";
+
 const Balance = () => {
   return (
     <section className="balance">
       <h2 className="balance_caption">My balance</h2>
       <span className="balance_value">$921.48</span>
-      <img src="images/logo.svg" alt="Logo" className="logo" />
+      <img src={logo} alt="Logo" className="logo" />
     </section>
   );
 };
