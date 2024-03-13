1c60760fed272e7248f951107e22a717339eaf71
Panji <panji9502@gmail.com>
2024-03-14 05:17:20 +0700

    Decrease bar height

--- expenses-chart/src/Bar.jsx
+++ expenses-chart/src/Bar.jsx
@@ -2,7 +2,7 @@ const Bar = ({ day, amount, height }) => {
   return (
     <div className="bar-container">
       <span className="bar_value">{amount}</span>
-      <div className="bar" style={{ height: `${height}px` }}></div>
+      <div className="bar" style={{ height: `${height * 0.7}px` }}></div>
       <span className="bar_subtitle">{day}</span>
     </div>
   );
