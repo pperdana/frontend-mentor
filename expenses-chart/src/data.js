f87b29afe026f1652ee6a9a3aebe75d4a1479399
Panji <panji9502@gmail.com>
2024-03-13 12:54:44 +0700

    Update data with height

--- expenses-chart/src/data.js
+++ expenses-chart/src/data.js
@@ -2,30 +2,37 @@ const data = [
   {
     day: "mon",
     amount: 17.45,
+    height: 50,
   },
   {
     day: "tue",
     amount: 34.91,
+    height: 100,
   },
   {
     day: "wed",
     amount: 52.36,
+    height: 150,
   },
   {
     day: "thu",
     amount: 31.07,
+    height: 89,
   },
   {
     day: "fri",
     amount: 23.39,
+    height: 67,
   },
   {
     day: "sat",
     amount: 43.28,
+    height: 124,
   },
   {
     day: "sun",
     amount: 25.48,
+    height: 73,
   },
 ];
 
