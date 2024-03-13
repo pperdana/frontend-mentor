35a97b60129fb76dddc44163fb363bed7cc7c4ce
Panji <panji9502@gmail.com>
2024-03-13 12:42:23 +0700

    Update GraphContainer content with map

--- expenses-chart/src/GraphContainer.jsx
+++ expenses-chart/src/GraphContainer.jsx
@@ -1,41 +1,11 @@
-const GraphContainer = () => {
+import Bar from "./Bar";
+
+const GraphContainer = ({ data }) => {
   return (
     <div className="graphic_container">
-      <div className="bar-container">
-        <span className="bar_value">$17.45</span>
-        <div className="bar"></div>
-        <span className="bar_subtitle">mon</span>
-      </div>
-      <div className="bar-container">
-        <span className="bar_value">$34.91</span>
-        <div className="bar"></div>
-        <span className="bar_subtitle">tue</span>
-      </div>
-      <div className="bar-container">
-        <span className="bar_value">$52.36</span>
-        <div className="bar"></div>
-        <span className="bar_subtitle">wed</span>
-      </div>
-      <div className="bar-container">
-        <span className="bar_value">$31.07</span>
-        <div className="bar"></div>
-        <span className="bar_subtitle">thu</span>
-      </div>
-      <div className="bar-container">
-        <span className="bar_value">$23.39</span>
-        <div className="bar"></div>
-        <span className="bar_subtitle">fri</span>
-      </div>
-      <div className="bar-container">
-        <span className="bar_value">$43.28</span>
-        <div className="bar"></div>
-        <span className="bar_subtitle">sat</span>
-      </div>
-      <div className="bar-container">
-        <span className="bar_value">$25.48</span>
-        <div className="bar"></div>
-        <span className="bar_subtitle">sun</span>
-      </div>
+      {data.map((item) => {
+        return <Bar key={item.day} {...item} />;
+      })}
     </div>
   );
 };
