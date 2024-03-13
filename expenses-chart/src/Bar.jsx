11a395b25f6b12740fcb7ba16827e8cb3a24cddc
Panji <panji9502@gmail.com>
2024-03-13 12:55:25 +0700

    Add dynamic height from data

--- expenses-chart/src/Bar.jsx
+++ expenses-chart/src/Bar.jsx
@@ -1,8 +1,8 @@
-const Bar = ({ day, amount }) => {
+const Bar = ({ day, amount, height }) => {
   return (
     <div className="bar-container">
       <span className="bar_value">{amount}</span>
-      <div className="bar"></div>
+      <div className="bar" style={{ height: `${height}px` }}></div>
       <span className="bar_subtitle">{day}</span>
     </div>
   );
