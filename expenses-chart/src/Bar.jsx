6929596cf79470135c46a2de23af6b0ee15253e7
Panji <panji9502@gmail.com>
2024-03-15 23:13:37 +0700

    Add onClick event listener

--- expenses-chart/src/Bar.jsx
+++ expenses-chart/src/Bar.jsx
@@ -2,7 +2,11 @@ const Bar = ({ day, amount, height, isActive, setIsActive }) => {
   return (
     <div className="bar-container">
       <span className="bar_value">{amount}</span>
-      <div className="bar" style={{ height: `${height * 0.7}px` }}></div>
+      <div
+        className="bar"
+        style={{ height: `${height * 0.7}px` }}
+        onClick={() => setIsActive(!isActive)}
+      ></div>
       <span className="bar_subtitle">{day}</span>
     </div>
   );
