a010173fd5416fc8d9ce53fd266f9aa05b2c903c
Panji <panji9502@gmail.com>
2024-03-19 10:43:09 +0700

    Add main styles

--- age-calculator-app/src/App.jsx
+++ age-calculator-app/src/App.jsx
@@ -4,7 +4,7 @@ import Result from "./Result";
 
 const App = () => {
   return (
-    <main>
+    <main className="py-[2.5rem] px-[3rem] w-full max-w-[4.2rem] bg-[hsl(0,0%,100%)] m-[1rem] rounded-[1.5rem] rounded-br-[6rem] shadow-[1px_1px_12px_0px_rgba(20,20,20,0.11)]">
       <Title />
       <Form />
       <Result />
