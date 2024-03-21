0e047c2a516f827e5d23b838102eac79650a9d1c
Panji <panji9502@gmail.com>
2024-03-21 23:20:52 +0700

    Resize styles

--- age-calculator-app/src/App.jsx
+++ age-calculator-app/src/App.jsx
@@ -4,7 +4,7 @@ import Result from "./Result";
 
 const App = () => {
   return (
-    <main className="py-[4rem] px-[4.8rem] w-full max-w-[67rem] bg-[hsl(0,0%,100%)] m-[1.6rem] rounded-[2.4rem] rounded-br-[9.6rem] shadow-[1px_1px_12px_0px_rgba(20,20,20,0.11)]">
+    <main className="py-[4rem] px-[4.8rem] w-full max-w-[63rem] bg-[hsl(0,0%,100%)] m-[1.6rem] rounded-[2.4rem] rounded-br-[13.5rem] shadow-[1px_1px_12px_0px_rgba(20,20,20,0.11)]">
       <Title />
       <Form />
       <Result />
