a467988249534a314915829a789337254df8142a
Panji <panji9502@gmail.com>
2024-03-25 11:17:37 +0700

    Create icons component

---
+++ fylo-data-storage/src/Icons.jsx
@@ -0,0 +1,22 @@
+const Icons = () => {
+  return (
+    <section className="icons">
+      <div className="logo">
+        <img src="./images/logo.svg" alt="" />
+      </div>
+      <div className="items">
+        <div className="document">
+          <img src="./images/icon-document.svg" alt="" />
+        </div>
+        <div className="folder">
+          <img src="./images/icon-folder.svg" alt="" />
+        </div>
+        <div className="upload">
+          <img src="./images/icon-upload.svg" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Icons;
