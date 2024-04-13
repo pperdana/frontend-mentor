2c995f829c72e1c3b9bb5408ed92e337ab4f82b2
Panji <panji9502@gmail.com>
2024-04-13 15:17:58 +0700

    Add components

--- time-tracking-dashboard/src/App.jsx
+++ time-tracking-dashboard/src/App.jsx
@@ -1,101 +1,11 @@
+import CardList from "./CardList";
+
+import data from "./data.js";
+
 const App = () => {
   return (
     <main className="main">
-      <div className="cards">
-        <div className="card profile">
-          <div className="content">
-            <img src="./images/image-jeremy.png" alt="" />
-            <div className="report">
-              Report for <span className="user">Jeremy Robson</span>
-            </div>
-          </div>
-          <div className="links">
-            <a href="#">Daily</a>
-            <a href="#" className="active">
-              Weekly
-            </a>
-            <a href="#">Monthly</a>
-          </div>
-        </div>
-
-        <div className="card work">
-          <div className="content">
-            <h2>Work</h2>
-            <button className="ellipses">
-              <img src="./images/icon-ellipsis.svg" alt="" />
-            </button>
-            <span className="current">32hrs</span>
-            <span className="previous">
-              Last <span>Week</span> - <span>36hrs</span>
-            </span>
-          </div>
-        </div>
-
-        <div className="card exercise">
-          <div className="content">
-            <h2>Exercise</h2>
-            <button className="ellipses">
-              <img src="./images/icon-ellipsis.svg" alt="" />
-            </button>
-            <span className="current">4hrs</span>
-            <span className="previous">
-              Last <span>Week</span> - <span>5hrs</span>
-            </span>
-          </div>
-        </div>
-
-        <div className="card play">
-          <div className="content">
-            <h2>Play</h2>
-            <button className="ellipses">
-              <img src="./images/icon-ellipsis.svg" alt="" />
-            </button>
-            <span className="current">10hrs</span>
-            <span className="previous">
-              Last <span>Week</span> - <span>8hrs</span>
-            </span>
-          </div>
-        </div>
-
-        <div className="card social">
-          <div className="content">
-            <h2>Social</h2>
-            <button className="ellipses">
-              <img src="./images/icon-ellipsis.svg" alt="" />
-            </button>
-            <span className="current">5hrs</span>
-            <span className="previous">
-              Last <span>Week</span> - <span>10hrs</span>
-            </span>
-          </div>
-        </div>
-
-        <div className="card study">
-          <div className="content">
-            <h2>Study</h2>
-            <button className="ellipses">
-              <img src="./images/icon-ellipsis.svg" alt="" />
-            </button>
-            <span className="current">4hrs</span>
-            <span className="previous">
-              Last <span>Week</span> - <span>7hrs</span>
-            </span>
-          </div>
-        </div>
-
-        <div className="card selfcare">
-          <div className="content">
-            <h2>Self Care</h2>
-            <button className="ellipses">
-              <img src="./images/icon-ellipsis.svg" alt="" />
-            </button>
-            <span className="current">2hrs</span>
-            <span className="previous">
-              Last<span>Week</span> - <span>2hrs</span>
-            </span>
-          </div>
-        </div>
-      </div>
+      <CardList data={data} />
     </main>
   );
 };
