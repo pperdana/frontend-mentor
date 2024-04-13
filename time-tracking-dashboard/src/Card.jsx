0f6b500be06d09e2515c79012db32a095e8e7ebe
Panji <panji9502@gmail.com>
2024-04-13 15:15:40 +0700

    Create card component

---
+++ time-tracking-dashboard/src/Card.jsx
@@ -0,0 +1,18 @@
+const Card = () => {
+  return (
+    <div className="card work">
+      <div className="content">
+        <h2>Work</h2>
+        <button className="ellipses">
+          <img src="./images/icon-ellipsis.svg" alt="" />
+        </button>
+        <span className="current">32hrs</span>
+        <span className="previous">
+          Last <span>Week</span> - <span>36hrs</span>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
