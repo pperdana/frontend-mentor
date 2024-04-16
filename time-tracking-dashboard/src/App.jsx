a8a306aea560c2bc5aec091cd78a17a413efbaad
Panji <panji9502@gmail.com>
2024-04-16 15:26:04 +0700

    Update content

--- time-tracking-dashboard/src/App.jsx
+++ time-tracking-dashboard/src/App.jsx
@@ -5,11 +5,9 @@ import data from "./data.js";
 const App = () => {
   return (
     <>
-      <h2>test</h2>
-      {/* <main className="main">
-      <h2>test</h2>
-      <CardList data={data} /> 
-    </main> */}
+      <main className="main">
+        <CardList data={data} />
+      </main>
     </>
   );
 };
