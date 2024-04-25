c7b4b109c5eb7fd4e275ef1515ef451d4467dfd6
Panji <panji9502@gmail.com>
2024-04-25 12:03:17 +0700

    Define lg screeen

--- newsletter-sign-up-form/tailwind.config.js
+++ newsletter-sign-up-form/tailwind.config.js
@@ -15,7 +15,7 @@ export default {
       // Screen for Tablet
       // tall: { raw: "(min-height: 900px)" },
       // Screen for various width
-      // lg: { max: "75em" },
+      lg: { max: "68.75em" },
       // md: { max: "42em" },
       // sm: { max: "28em" },
     },
