d1e3d690117cd852e349009a03d7fb7b233e37a6
Panji <panji9502@gmail.com>
2024-05-02 11:40:12 +0700

    Create feature component

---
+++ fylo-dark-theme/src/Feature.jsx
@@ -0,0 +1,43 @@
+const Feature = () => {
+  return (
+    <section className="section-2 section-featured">
+      <div>
+        <img src="images/icon-access-anywhere.svg" alt="icon-access-anywhere" />
+        <h2>Access your files, anywhere</h2>
+        <p>
+          The ability to use a smartphone, tablet, or computer to access your
+          account means your files follow you everywhere.
+        </p>
+      </div>
+
+      <div>
+        <img src="images/icon-security.svg" alt="icon-security" />
+        <h2>Security you can trust</h2>
+        <p>
+          2-factor authentication and user-controlled encryption are just a
+          couple of the security features we allow to help secure your files.
+        </p>
+      </div>
+
+      <div>
+        <img src="images/icon-collaboration.svg" alt="icon-collaboration" />
+        <h2>Real-time collaboration</h2>
+        <p>
+          Securely share files and folders with friends, family and colleagues
+          for live collaboration. No email attachments required.
+        </p>
+      </div>
+
+      <div>
+        <img src="images/icon-any-file.svg" alt="icon-any-file" />
+        <h2>Store any type of file</h2>
+        <p>
+          Whether you're sharing holidays photos or work documents, Fylo has you
+          covered allowing for all file types to be securely stored and shared.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Feature;
