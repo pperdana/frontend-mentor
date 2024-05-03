c55b4b97349248c060b35ff02c2d8338a0ebc846
Panji <panji9502@gmail.com>
2024-05-04 01:28:21 +0700

    Define font

--- fylo-dark-theme/tailwind.config.js
+++ fylo-dark-theme/tailwind.config.js
@@ -8,7 +8,8 @@ export default {
       sm: "50%",
     },
     fontFamily: {
-      // poppins: ["Poppins", "sans-serif"],
+      open: ["Open Sans", "sans-serif"],
+      raleway: ["Raleway", "sans-serif"],
     },
     extend: {},
     screens: {
