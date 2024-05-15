7cc6668162052affb1f8f9263de3888b4a73085b
Panji <panji9502@gmail.com>
2024-05-15 09:21:42 +0700

    Add class name

--- fylo-dark-theme/src/Testimonials.jsx
+++ fylo-dark-theme/src/Testimonials.jsx
@@ -7,46 +7,46 @@ const Testimonials = () => {
     <section className="section-4 section-testimonials">
       <main>
         <div className="testimonials">
-          <p>
+          <p className="testimonials-desc">
             Fylo has improved our team productivity by an order of magnitude.
             Since making the switch our team has become a well-oiled
             collaboration machine.
           </p>
           <div className="avatar-div">
-            <img src={profile1Img} alt="profile-1" />
+            <img className="avatar-img" src={profile1Img} alt="profile-1" />
             <div>
               <h4>Satish Patel</h4>
-              <p>Founder &amp; CEO, Huddle</p>
+              <p className="position">Founder &amp; CEO, Huddle</p>
             </div>
           </div>
         </div>
 
         <div className="testimonials">
-          <p>
+          <p className="testimonials-desc">
             Fylo has improved our team productivity by an order of magnitude.
             Since making the switch our team has become a well-oiled
             collaboration machine.
           </p>
           <div className="avatar-div">
-            <img src={profile2Img} alt="profile-2" />
+            <img className="avatar-img" src={profile2Img} alt="profile-2" />
             <div>
               <h4>Bruce McKenzie</h4>
-              <p>Founder &amp; CEO, Huddle</p>
+              <p className="position">Founder &amp; CEO, Huddle</p>
             </div>
           </div>
         </div>
 
         <div className="testimonials">
-          <p>
+          <p className="testimonials-desc">
             Fylo has improved our team productivity by an order of magnitude.
             Since making the switch our team has become a well-oiled
             collaboration machine.
           </p>
           <div className="avatar-div">
-            <img src={profile3Img} alt="profile-3" />
+            <img className="avatar-img" src={profile3Img} alt="profile-3" />
             <div>
               <h4>Iva Boyd</h4>
-              <p>Founder &amp; CEO, Huddle</p>
+              <p className="position">Founder &amp; CEO, Huddle</p>
             </div>
           </div>
         </div>
