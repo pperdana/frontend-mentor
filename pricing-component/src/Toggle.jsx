b8133a1947fad738786fff72a0de207aa2b7cc06
Panji <panji9502@gmail.com>
2024-05-21 14:39:36 +0700

    Create toggle component

---
+++ pricing-component/src/Toggle.jsx
@@ -0,0 +1,13 @@
+const Toggle = () => {
+  return (
+    <div className="toggle_div">
+      <span className="annually">Annually</span>
+      <div className="toogle_btn">
+        <div className="toggle"></div>
+      </div>
+      <span className="month">Monthly</span>
+    </div>
+  );
+};
+
+export default Toggle;
