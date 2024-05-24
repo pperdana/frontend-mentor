b1f3d7dc5339cef4d878c1a543e802355df46305
Panji <panji9502@gmail.com>
2024-05-25 03:02:42 +0700

    Starter contents

--- intro-with-dropdown/src/App.jsx
+++ intro-with-dropdown/src/App.jsx
@@ -1,3 +1,5 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  return <h1 className="font-bold underline">Hello world!</h1>;
+};
+
+export default App;
