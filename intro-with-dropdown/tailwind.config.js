3e2716be3177b4774a9f955467629d8d93184aed
Panji <panji9502@gmail.com>
2024-05-25 03:02:07 +0700

    Starter styles

--- intro-with-dropdown/tailwind.config.js
+++ intro-with-dropdown/tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      xl: "62.5%",
+      base: "56.25%",
+      sm: "50%",
+    },
+    fontFamily: {
+      // poppins: ["Poppins", "sans-serif"],
+    },
     extend: {},
+    screens: {
+      // Screen for Tablet
+      // tall: { raw: "(min-height: 900px)" },
+      // Screen for various width
+      // lg: { max: "75em" },
+      // md: { max: "42em" },
+      // sm: { max: "28em" },
+    },
   },
   plugins: [],
-}
+};
