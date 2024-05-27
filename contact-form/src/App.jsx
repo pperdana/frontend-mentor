bc91724feb14cdcc9367967548876904634b9d6a
Panji <panji9502@gmail.com>
2024-05-27 11:54:06 +0700

    Add contents

--- contact-form/src/App.jsx
+++ contact-form/src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="alert" style="display: none">
+        <i className="fa-regular fa-circle-check">
+          <span className="msg">Message Sent!</span>
+        </i>
+        <h4>Thanks for completing the form. We'll be in touch soon!</h4>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div id="form-container">
+      <div class="form-box">
+        <h1>Contact Us</h1>
+        <form action="#" name="forms" id="form">
+          <div class="input-group">
+            <div class="row">
+              <div class="form-control name">
+                <label for="name">First Name *</label>
+                <input type="text" id="name" class="change">
+                <p id="nameError" class="error"></p>
+              </div>
+              <div class="form-control name">
+                <label for="last_name" class="last">Last Name *</label>
+                <input type="text" id="last_name" class="change">
+                <p id="lastnameError" class="error"></p>
+              </div>
+            </div>
+            
 
-export default App
+            <div class="form-control">
+              <label for="email">Email Address *</label>
+              <input type="text" id="email" class="change">
+              <p id="emailError" class="error"></p>
+            </div>
+
+            <div class="form-control">
+              <p>Query Type *</p>
+              <div class="inline">
+                <div class="radio">
+                  <input type="radio" name="radio" id="small">
+                  <label for="small" class="border">Gender Enquiry</label>
+                </div>
+                
+                <div class="radio">
+                  <input type="radio" name="radio" id="large">
+                  <label for="large" class="border">Support Request</label>
+                </div>
+                
+              </div>
+              
+              <p id="Queryerror" class="error" style="display: none">
+                Please select a query type
+              </p>
+            </div>
+
+            <div class="form-control">
+              <label for="message">Message *</label> <br>
+              <textarea name="message" id="message" rows="6" cols="75"></textarea>
+              <p id="Messageerror" class="error"></p>
+            </div>
+
+            <div class="form-control check">
+              <input type="checkbox" id="mycheck" name="checked">
+              <label for="mycheck">I consent to being contacted by the team *</label>
+            </div>
+            <p id="Checkerror" class="error" style="display: none">
+              To submit this form,please consent to being contacted
+            </p>
+          </div>
+          
+
+          <button type="submit" id="btn">Submit</button>
+        </form>
+      </div>
+      
+    </div>
+    </>
+  );
+};
+
+export default App;
