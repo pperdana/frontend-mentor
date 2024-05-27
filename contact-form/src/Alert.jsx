b68b3da4f423bc7caa6016952e4e01ef6d0f2acd
Panji <panji9502@gmail.com>
2024-05-27 12:11:51 +0700

    Update alert content

--- contact-form/src/Alert.jsx
+++ contact-form/src/Alert.jsx
@@ -1,11 +1,13 @@
 const Alert = () => {
   return (
-    <div className="alert" style="display: none">
-      <i className="fa-regular fa-circle-check">
-        <span className="msg">Message Sent!</span>
-      </i>
-      <h4>Thanks for completing the form. We'll be in touch soon!</h4>
-    </div>
+    <>
+      <div className="alert">
+        <i className="fa-regular fa-circle-check">
+          <span className="msg">Message Sent!</span>
+        </i>
+        <h4>Thanks for completing the form. We'll be in touch soon!</h4>
+      </div>
+    </>
   );
 };
 
