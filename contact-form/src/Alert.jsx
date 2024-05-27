d487dea11b7313e2f49edd32561451ca048cb1c1
Panji <panji9502@gmail.com>
2024-05-27 11:55:47 +0700

    Create alert component

---
+++ contact-form/src/Alert.jsx
@@ -0,0 +1,12 @@
+const Alert = () => {
+  return (
+    <div className="alert" style="display: none">
+      <i className="fa-regular fa-circle-check">
+        <span className="msg">Message Sent!</span>
+      </i>
+      <h4>Thanks for completing the form. We'll be in touch soon!</h4>
+    </div>
+  );
+};
+
+export default Alert;
