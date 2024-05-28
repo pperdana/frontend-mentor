96a8cdcb9ef39d2c12496613823df10f0888e765
Panji <panji9502@gmail.com>
2024-05-28 07:16:22 +0700

    Rename component

---
+++ contact-form/src/FormContainer.jsx
@@ -0,0 +1,14 @@
+import Form from "./Form";
+
+const FormContainer = () => {
+  return (
+    <div id="form-container">
+      <div className="form-box">
+        <h1>Contact Us</h1>
+        <Form />
+      </div>
+    </div>
+  );
+};
+
+export default FormContainer;
