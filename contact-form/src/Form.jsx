fe4be44ed0bb3f66aefc15fedec18a6c9545fc69
Panji <panji9502@gmail.com>
2024-05-28 07:49:11 +0700

    Update form styles

--- contact-form/src/Form.jsx
+++ contact-form/src/Form.jsx
@@ -1,6 +1,8 @@
+import styled from "styled-components";
+
 const Form = () => {
   return (
-    <form action="#" name="forms" id="form">
+    <Wrapper action="#" name="forms" id="form">
       <div className="input-group">
         <div className="row">
           <div className="form-control name">
@@ -22,7 +24,7 @@ const Form = () => {
         </div>
 
         <div className="form-control">
-          <p>Query Type *</p>
+          <p className="radio-query">Query Type *</p>
           <div className="inline">
             <div className="radio">
               <input type="radio" name="radio" id="small" />
@@ -55,11 +57,52 @@ const Form = () => {
         </p>
       </div>
 
-      <button type="submit" id="btn">
+      <button type="submit" id="btn" className="btn">
         Submit
       </button>
-    </form>
+    </Wrapper>
   );
 };
 
 export default Form;
+
+const Wrapper = styled.form`
+  .row {
+    display: flex;
+    justify-content: space-between;
+    gap: 1.6rem;
+  }
+  .check {
+    margin-top: 25px;
+    display: flex;
+    gap: 20px;
+  }
+  .radio-query {
+    font-size: 1.6rem;
+  }
+  .error {
+    display: none;
+  }
+  .change {
+    cursor: pointer;
+    width: 100%;
+    margin: 10px 0px;
+    padding: 10px;
+    border-radius: 5px;
+    border: 1.5px solid hsl(186, 15%, 59%);
+    outline: none;
+  }
+  .btn {
+    margin-top: 30px;
+    width: 108%;
+    font-size: 1.9rem;
+    background: hsl(169, 82%, 27%);
+    color: #fff;
+    font-weight: 500;
+    height: 40px;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+    cursor: pointer;
+  }
+`;
