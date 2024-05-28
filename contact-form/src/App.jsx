95bfd29a29abed92f54cdc17ce65821c32e59b28
Panji <panji9502@gmail.com>
2024-05-28 07:08:28 +0700

    Add main styles

--- contact-form/src/App.jsx
+++ contact-form/src/App.jsx
@@ -1,13 +1,23 @@
+import styled from "styled-components";
+
 import Alert from "./Alert";
 import Form from "./Form";
 
 const App = () => {
   return (
-    <>
+    <Wrapper>
       <Alert />
       <Form />
-    </>
+    </Wrapper>
   );
 };
 
 export default App;
+
+const Wrapper = styled.main`
+  height: 100vh;
+  background-color: hsl(148, 38%, 91%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
