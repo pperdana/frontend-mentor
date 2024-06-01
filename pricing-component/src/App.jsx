ee3430c47f9e52b796881cf0d14c2bcbd950fb71
Panji <panji9502@gmail.com>
2024-06-01 15:28:31 +0700

    Set state

--- pricing-component/src/App.jsx
+++ pricing-component/src/App.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
+
 import Title from "./Title";
 import Toggle from "./Toggle";
 import Content from "./Content";
 
 const App = () => {
+  const [monthly, setMonthly] = useState(true);
   return (
     <main>
       <div className="main_content">
         <Title />
-        <Toggle />
-        <Content />
+        <Toggle monthly={monthly} setMonthly={setMonthly} />
+        <Content monthly={monthly} />
       </div>
     </main>
   );
