bb07e124837ff0c6205cf7d4e1fe734b0b2d1968
Panji <panji9502@gmail.com>
2024-06-06 08:45:29 +0700

    Add design styles

--- blogr-landing-page/src/Design.jsx
+++ blogr-landing-page/src/Design.jsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
+
 const Design = () => {
   return (
-    <div id="Design">
+    <Wrapper id="Design">
       <h2 className="heading">Designed for the future</h2>
       <div id="main-txt-img">
         <div id="left">
@@ -18,12 +19,12 @@ const Design = () => {
           </div>
           <div id="management">
             <h3>Robust content management</h3>
-            <div className="para">
+            <p className="para">
               Flexible content management enables users to easily move through
               posts. Increase the usability of your blog by adding customized
               categories, sections, format, or flow. With this functionality,
               you're in full control.
-            </div>
+            </p>
           </div>
         </div>
         <div id="right">
@@ -39,8 +40,47 @@ const Design = () => {
           />
         </div>
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
 export default Design;
+
+const Wrapper = styled.div`
+  margin-block: max(3vw, 1rem) 0;
+
+  h2 {
+    font-weight: 500;
+    text-align: center;
+    margin-block: 13vh 0;
+    font-family: var(--Ubuntu);
+    font-size: max(2.8dvw, 1.45rem);
+    color: var(--Very-dark-blue-heading);
+  }
+
+  #main-txt-img {
+    width: 100%;
+    display: flex;
+  }
+  #main-txt-img #left {
+    width: 50%;
+    margin-top: 10dvw;
+    padding-left: 10vw;
+  }
+  #editor {
+    margin-block: 1dvh max(5dvw, 3rem);
+  }
+  h3 {
+    font-weight: 500;
+    font-size: 1.6rem;
+    margin-bottom: 6vh;
+    font-family: var(--Ubuntu);
+    color: var(--Very-dark-blue-heading);
+  }
+  #editor .para {
+    width: 83.9%;
+  }
+  #main-txt-img #right {
+    width: 50%;
+  }
+`;
