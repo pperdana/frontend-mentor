6af74e2ec9925760acd17222131d5b39fe8700c1
Panji <panji9502@gmail.com>
2024-06-26 14:13:39 +0700

    cta content

--- huddle-landing-page/src/App.jsx
+++ huddle-landing-page/src/App.jsx
@@ -136,6 +136,13 @@ const App = () => {
           alt=""
         />
       </section>
+
+      <section className="text-center my-5">
+        <h1 className="urban my-5">Ready To build Your Community</h1>
+        <button className="text-capitalize py-3 px-4 rounded-5 fw-bold buttonhero my-5">
+          Get Started
+        </button>
+      </section>
     </>
   );
 };
