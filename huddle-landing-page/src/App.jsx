7108292d745c2d6fdb25cc849d3541e386a00d26
Panji <panji9502@gmail.com>
2024-06-26 14:03:58 +0700

    navbar content

--- huddle-landing-page/src/App.jsx
+++ huddle-landing-page/src/App.jsx
@@ -1,5 +1,22 @@
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <nav className="navbar">
+        <div className="container py-5 flex-nowrap">
+          <a className="navbar-brand" href="#">
+            <img
+              className="img-fluid logo"
+              src="./images/logo.svg"
+              alt="Bootstrap"
+            />
+          </a>
+          <button className="text-capitalize rounded-5 fw-bold navbutton">
+            try it free
+          </button>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default App;
