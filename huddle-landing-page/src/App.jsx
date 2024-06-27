df5007e6d8cac2f306d9ded61591c619be7d5b5c
Panji <panji9502@gmail.com>
2024-06-27 15:16:03 +0700

    Import header component

--- huddle-landing-page/src/App.jsx
+++ huddle-landing-page/src/App.jsx
@@ -1,22 +1,11 @@
+import Header from "./Header";
 import Cta from "./Cta";
 import Footer from "./Footer";
+
 const App = () => {
   return (
     <>
-      <nav className="navbar">
-        <div className="container py-5 flex-nowrap">
-          <a className="navbar-brand" href="#">
-            <img
-              className="img-fluid logo"
-              src="./images/logo.svg"
-              alt="Bootstrap"
-            />
-          </a>
-          <button className="text-capitalize rounded-5 fw-bold navbutton">
-            try it free
-          </button>
-        </div>
-      </nav>
+      <Header />
 
       <section className="text-center">
         <h1 className="my-5 fw-bold">
