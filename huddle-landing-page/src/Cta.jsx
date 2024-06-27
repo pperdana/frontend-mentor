0cb5db533a9b3f8a3294f72f893f9bf9b31a2a5f
Panji <panji9502@gmail.com>
2024-06-27 15:14:37 +0700

    Create cta component

---
+++ huddle-landing-page/src/Cta.jsx
@@ -0,0 +1,12 @@
+const Cta = () => {
+  return (
+    <section className="text-center my-5">
+      <h1 className="urban my-5">Ready To build Your Community</h1>
+      <button className="text-capitalize py-3 px-4 rounded-5 fw-bold buttonhero my-5">
+        Get Started
+      </button>
+    </section>
+  );
+};
+
+export default Cta;
