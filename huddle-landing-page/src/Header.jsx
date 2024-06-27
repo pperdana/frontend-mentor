40fcbd1bfdeafae9cd41c30a76f97c3215937817
Panji <panji9502@gmail.com>
2024-06-27 15:15:57 +0700

    Create header component

---
+++ huddle-landing-page/src/Header.jsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <nav className="navbar">
+      <div className="container py-5 flex-nowrap">
+        <a className="navbar-brand" href="#">
+          <img
+            className="img-fluid logo"
+            src="./images/logo.svg"
+            alt="Bootstrap"
+          />
+        </a>
+        <button className="text-capitalize rounded-5 fw-bold navbutton">
+          try it free
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
